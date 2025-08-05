fe4466119a4a9caeeddfbef3bbf6e9d8b48eff08
Konstantinov <a.konstantinov@launchoptions.by>
2025-08-05 10:31:29 +0300

    add

---
+++ Sources/CoreSdkPackage/SdkHelper.swift
@@ -0,0 +1,14 @@
+import CoreSdk
+import SwiftUI
+
+public class SdkHelper {
+    @MainActor public static let shared = SdkHelper()
+
+    private init() {}
+    
+    @MainActor @available(iOS 13.0, *)
+    public func makeUIViewController() -> UIViewController {
+        return MainKt.MainViewController()
+    }
+}
+

60bb2e88bfa2e4d02651967dc7dec37c70f69e32
Konstantinov <a.konstantinov@launchoptions.by>
2025-08-05 10:43:25 +0300

    add

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CoreSdk",
-            url: "https://github.com/AndrewKonst/TestPackage/releases/download/1.0.0/CoreSdk.xcframework.zip",
+            url: "https://github.com/AndrewKonst/TestPackage/releases/download/v1.0.0/CoreSdk.xcframework.zip",
             checksum: "e214d0656135e635a9d664fc9a89585ae37c9731e6dfa6e3f5527373afe796eb"
         )
     ]
